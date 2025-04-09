124c1c4f385b3730a48265fa7095056a2c3cd874
nhatminh291091 <130354200+nhatminh291091@users.noreply.github.com>
2025-04-09 20:17:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -57,5 +57,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 module.exports = app;
